71ae98917d66be7d7a7bd0f80ece903417e0fab5
DionisRaci <31957008+DionisRaci@users.noreply.github.com>
2017-11-30 19:33:33 +0100

    Car.cpp & car.h

--- car.h
+++ car.h
@@ -1,10 +1,10 @@
 /*----------------------------------------------------------
 *				HTBLA-Leonding / Klasse: <your class>
 * ---------------------------------------------------------
-* Exercise Number: 0
+* Exercise Number: 5
 * Title:			car.h
-* Author:			P. Bauer
-* Due Date:		January 9, 2015
+* Author:			Dionis Raci
+* Due Date:		november 24, 2017
 * ----------------------------------------------------------
 * Description:
 * Car abstract data type demo.
@@ -12,5 +12,24 @@
 */
 #ifndef ___CAR_H
 #define ___CAR_H
+
+enum type{
+    AIXAM, FIAT_MULTIPLA, JEEP
+};
+enum color{
+  RED, GREEN, BLACK, SILVER, BLUE, ORANGE
+};
+
+typedef struct CarType* Car;
+
+Car get_car(type type);
+type get_type(Car car);
+color get_color(Car car);
+int get_fill_level(Car car);
+double get_acceleration_rate(Car car);
+int get_speed(Car car);
+void set_acceleration_rate(Car car, double acceleration_rate);
+void accelerate(Car car);
+void init();
 
 #endif
